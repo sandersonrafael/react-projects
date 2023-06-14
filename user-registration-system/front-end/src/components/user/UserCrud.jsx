d4c21179471c9436de0f5ef4a84aad17063b179e
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-14 13:32:58 -0300

    crud finished

--- user-registration-system/front-end/src/components/user/UserCrud.jsx
+++ user-registration-system/front-end/src/components/user/UserCrud.jsx
@@ -20,15 +20,21 @@ const UserCrud = () => {
   const inputNameRef = useRef(null);
   const inputEmailRef = useRef(null);
 
+  useEffect(() => {
+    axios(baseUrl).then((res) => {
+      setState({ ...state, list: res.data });
+    });
+  }, []); // eslint-disable-line
+
   const clear = () => {
     inputNameRef.current.value = '';
     inputEmailRef.current.value = '';
     setState({ ...state, user: initialState.user });
   };
 
-  const getUpdatedList = (user) => {
+  const getUpdatedList = (user, add = true) => {
     const list = state.list.filter((u) => u.id !== user.id);
-    list.unshift(user);
+    if (add) list.unshift(user);
     return list;
   };
 
@@ -36,6 +42,7 @@ const UserCrud = () => {
     const user = { ...state.user };
     user.name = inputNameRef.current.value;
     user.email = inputEmailRef.current.value;
+    console.log(state.list);
     return setState({ ...state, user });
   };
 
@@ -72,11 +79,11 @@ const UserCrud = () => {
       <hr />
 
       <div className="row">
-        <div className="col-12 d-flex justify-content-end">
+        <div className="col-12 d-flex justify-content-end gap-2">
           <button className="btn btn-primary" onClick={(e) => save(e)}>
             Salvar
           </button>
-          <button className="btn btn-secondary ml-6" onClick={(e) => clear(e)}>
+          <button className="btn btn-secondary" onClick={(e) => clear(e)}>
             Cancelar
           </button>
         </div>
@@ -86,15 +93,73 @@ const UserCrud = () => {
 
   const save = () => {
     const user = state.user;
+
+    if (!user.name || !user.email) return;
+
     const method = user.id ? 'put' : 'post';
     const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
     axios[method](url, user).then((res) => {
       const list = getUpdatedList(res.data);
       setState({ ...initialState, list });
     });
+
+    inputNameRef.current.value = '';
+    inputEmailRef.current.value = '';
   };
 
-  return <Main {...headerProps}>{renderForm()}</Main>;
+  const load = (user) => {
+    setState({ ...state, user });
+    inputNameRef.current.value = user.name;
+    inputEmailRef.current.value = user.email;
+  };
+
+  const remove = (user) => {
+    axios.delete(`${baseUrl}/${user.id}`).then((res) => {
+      const list = getUpdatedList(user, false);
+      setState({ ...state, list });
+    });
+  };
+
+  const renderRows = () => {
+    return state.list.map((user) => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td className="d-flex gap-2">
+            <button className="btn btn-warning" onClick={() => load(user)}>
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button className="btn btn-danger" onClick={() => remove(user)}>
+              <i className="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  const renderTable = () => (
+    <table className="table mt-4">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>E-mail</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </table>
+  );
+
+  return (
+    <Main {...headerProps}>
+      {renderForm()}
+      {renderTable()}
+    </Main>
+  );
 };
 
 export default UserCrud;
