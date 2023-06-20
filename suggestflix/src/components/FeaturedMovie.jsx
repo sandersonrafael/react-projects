5847be3d5912713fd8b32a8bed48351155e933ba
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-19 21:21:48 -0300

    att

--- suggestflix/src/components/FeaturedMovie.jsx
+++ suggestflix/src/components/FeaturedMovie.jsx
@@ -1,6 +1,13 @@
 import './FeaturedMovie.css';
 
 export default function FeaturedMovie({ item }) {
+  console.log(item);
+  const firstDate = new Date(item.first_air_date).getFullYear();
+  const genres = [];
+  for (let genre of item.genres) { // eslint-disable-line
+    genres.push(genre.name);
+  }
+
   return (
     <section
       className="featured"
@@ -10,7 +17,38 @@ export default function FeaturedMovie({ item }) {
         backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
       }}
     >
-      <div className="featured--vertical" />
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">
+            {item.name ?? item.original_name}
+          </div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average} pontos</div>
+            <div className="featured--year">{firstDate}</div>
+            <div className="featured--seasons">
+              {item.number_of_seasons}
+              {item.number_of_seasons === 1 ? ' temporada' : ' temporadas'}
+            </div>
+            <div className="featured--description">{item.overview}</div>
+            <div className="featured--buttons">
+              <a
+                href={item.homepage}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="featured--watchbutton"
+              >
+                ▶ Assistir
+              </a>
+              <a href="/#" className="featured--mylistbutton">
+                + Minha Lista
+              </a>
+            </div>
+            <div className="featured--genres">
+              <strong>Gêneros: {genres.join(', ')}</strong>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
