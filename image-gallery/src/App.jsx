e89ec3a3c33596408e229fa03d2751fefd8559f4
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-07-03 17:21:13 -0300

    commit inicial

---
+++ image-gallery/src/App.jsx
@@ -0,0 +1,16 @@
+import * as C from './App.styles';
+
+function App() {
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/* Área de Upload */}
+        {/* Área de Fotos */}
+      </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
---
+++ image-gallery/src/App.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #27282f;
+  color: #fff;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 30px 0;
+`;
+
+export const Header = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`;
